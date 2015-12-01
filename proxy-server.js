03fa1ae99472ca7ca845ba4ba055c16220a3d17d
GauriGNaik <gaurinaik.145@gmail.com>
2015-12-01 16:00:11 -0500

    added

--- proxy-server.js
+++ proxy-server.js
@@ -11,7 +11,7 @@ var count = 1;
 var prod = process.argv[2];
 var canary = process.argv[3];
 
-var socket = sioc('http://'+canary+':3001');
+var socket = sioc('http://'+canary+':3003');
 
 socket.on("heartbeat", function(client) {
     status = client.status;
@@ -25,9 +25,9 @@ socket.on("heartbeat", function(client) {
 var server = http.createServer(function(req, res) {
    
         if((count % 4 == 0) && (status == 'false')) { 
-          var target = "http://" + canary + ":3000";   
+          var target = "http://" + canary + ":3002";   
           proxy.web(req, res, {target: target});
-          console.log("Request forwarded to  server: http://" + canary + ":3000");
+          console.log("Request forwarded to  server: http://" + canary + ":3002");
           count = 1; 
 		}
 		else {
